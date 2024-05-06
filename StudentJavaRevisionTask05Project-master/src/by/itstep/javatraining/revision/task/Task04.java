ff300121544ea00b9dbb401c6ae289875a3764c5
User <User@User-PC>
2024-05-06 13:11:27 +0300

    Task 4 solved

--- StudentJavaRevisionTask05Project-master/src/by/itstep/javatraining/revision/task/Task04.java
+++ StudentJavaRevisionTask05Project-master/src/by/itstep/javatraining/revision/task/Task04.java
@@ -34,7 +34,11 @@ package by.itstep.javatraining.revision.task;
  */
 
 public class Task04 {
-    public static boolean start(int year) {
-        return false;
-    }
+	public static boolean start(int year) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			return true;
+		}
+
+		return false;
+	}
 }
