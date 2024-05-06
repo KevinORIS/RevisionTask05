41cc86565cea7fd35a5ec5ff7d7dea560ca17128
User <User@User-PC>
2024-05-06 13:17:22 +0300

    Task 5 solved

--- StudentJavaRevisionTask05Project-master/src/by/itstep/javatraining/revision/task/Task05.java
+++ StudentJavaRevisionTask05Project-master/src/by/itstep/javatraining/revision/task/Task05.java
@@ -41,7 +41,34 @@ package by.itstep.javatraining.revision.task;
  */
 
 public class Task05 {
-    public static int start(int month, int year) {
-        return 0;
-    }
+	public static int start(int month, int year) {
+		if (month < 1 || month > 12) {
+			return 0;
+		}
+
+		int day = 0;
+
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			day = 31; break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			day = 30; break;
+		case 2:
+			if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+				day = 29;
+			} else {
+				day = 28;
+			}
+		}
+		return day;
+	}
 }
